73283bd87814db77f95ca7a0ed1941fdff532b19
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-03-12 23:09:28 +0100

    started first screen

---
+++ CustomText.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.System;
+using SFML.Graphics;
+using System.Linq;
+
+namespace Spleef
+{
+    public class CustomText : Transformable, Drawable
+    {
+        public static Image FontImage = new Image("assets/images/font.png");
+        public static Texture FontTexture = new Texture(FontImage);
+
+        public CustomText()
+        {
+            Height = 10;
+            Color = Color.White;
+            Text = "";
+        }
+
+        public CustomText(string text) : this()
+        {
+            Text = text;
+        }
+
+        public Color Color { get; set; }
+        public FloatRect GlobalBounds => Transform.TransformRect(LocalBounds);
+        public int Height { get; set; }
+
+        public FloatRect LocalBounds
+        {
+            get
+            {
+                var max_x = 0f;
+                var offset = new Vector2f();
+                foreach (var item in Text)
+                {
+                    if (item == '\n')
+                    {
+                        offset.Y += Height;
+                        offset.X = 0;
+                    }
+                    else if (item == ' ')
+                    {
+                        offset.X += Height * 4f / 10;
+                    }
+                    else
+                    {
+                        var texRect = GetCharRect(item);
+                        offset.X += texRect.Width * Height / 10f - Height / 10f;
+                    }
+                    max_x = Math.Max(max_x, offset.X);
+                }
+                return new FloatRect(0, 0, max_x, offset.Y + 10);
+            }
+        }
+
+        public string Text { get; set; }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            states.Transform *= Transform;
+            var offset = new Vector2f();
+            var rect = new RectangleShape() { Texture = FontTexture, FillColor = Color };
+            foreach (var item in Text)
+            {
+                if (item == '\n')
+                {
+                    offset.Y += Height;
+                    offset.X = 0;
+                }
+                else if (item == ' ')
+                {
+                    offset.X += Height * 4f / 10;
+                }
+                else
+                {
+                    rect.Position = offset;
+                    var texRect = GetCharRect(item);
+                    rect.Size = new Vector2f(texRect.Width * Height / 10f, Height);
+                    rect.TextureRect = texRect;
+                    target.Draw(rect, states);
+                    offset.X += rect.Size.X - Height / 10f;
+                }
+            }
+        }
+
+        private static IntRect GetCharRect(char c)
+        {
+            if ("éèêë".Contains(c))
+                c = 'e';
+            if ("ÉÊËÈ".Contains(c))
+                c = 'e';
+            if (c == 'à')
+                c = 'a';
+            if (c == 'ç')
+                c = 'c';
+            c = char.ToLower(c);
+            var index = "abcdefghijklmnopqrstuvwxyz!?.+-:,".IndexOf(c);
+            if (index == -1)
+                return default;
+            int x = 0;
+            while (index >= 0)
+            {
+                if (FontImage.GetPixel((uint)x, 10).R == 0)
+                    index--;
+                x++;
+            }
+            var result = new IntRect(x - 1, 0, 0, 10);
+            index = x;
+            while (index < FontImage.Size.X && FontImage.GetPixel((uint)index, 10).R == 255)
+                index++;
+            result.Width = index - x + 1;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Spleef
+{
+    internal class Program
+    {
+        public static RenderWindow App;
+        public static Texture SparkTexture = new Texture("assets/images/spark.png") { Smooth = true };
+
+        private static void Main(string[] args)
+        {
+            App = new RenderWindow(new VideoMode(900, 600), "SPLEEF!", Styles.Close);
+            App.SetVerticalSyncEnabled(true);
+            App.Closed += (sender, e) => App.Close();
+            var title = new RectangleShape(new Vector2f(43, 26) * 400 / 43) { Texture = new Texture("assets/images/spleef.png") };
+            title.Origin = title.Size / 2;
+            title.Position = (Vector2f)App.Size / 2;
+            var startMessage = new CustomText("Appuyer sur une touche") { Height = 20 };
+            startMessage.Origin = new Vector2f(startMessage.LocalBounds.Width / 2, 10);
+            startMessage.Position = new Vector2f(App.Size.X / 2, 560);
+            bool bigger = true;
+            var clock = new Clock();
+            var darker = true;
+            var bgGreen = .6f;
+            var spark = new RectangleShape(new Vector2f(startMessage.LocalBounds.Width, startMessage.LocalBounds.Width)) { Texture = SparkTexture };
+            spark.Origin = spark.Size / 2;
+            var shrinker = Transform.Identity;
+            shrinker.Translate(startMessage.Position);
+            shrinker.Scale(1.5f, 20f / startMessage.LocalBounds.Width * 2.5f);
+            var rotation = 0f;
+
+            while (App.IsOpen)
+            {
+                App.DispatchEvents();
+
+                var delta = clock.Restart();
+
+                if (bigger)
+                {
+                    title.Scale *= 1 + .05f * delta.AsSeconds();
+                    startMessage.Scale *= 1 + .2f * delta.AsSeconds();
+                    if (startMessage.Scale.X > 1.2f)
+                        bigger = false;
+                }
+                else
+                {
+                    title.Scale /= 1 + .05f * delta.AsSeconds();
+                    startMessage.Scale /= 1 + .2f * delta.AsSeconds();
+                    if (startMessage.Scale.X < 1)
+                        bigger = true;
+                }
+
+                if (darker)
+                {
+                    bgGreen /= 1 + .2f * delta.AsSeconds();
+                    if (bgGreen < .3f)
+                        darker = false;
+                }
+                else
+                {
+                    bgGreen *= 1 + .2f * delta.AsSeconds();
+                    if (bgGreen > .65f)
+                        darker = true;
+                }
+                rotation += delta.AsSeconds() * 60;
+                spark.Rotation = rotation;
+                spark.Scale = startMessage.Scale;
+                clock.Restart();
+
+                App.Clear(new Color(255, (byte)(bgGreen * 255), 0));
+
+                App.Draw(spark, new RenderStates(shrinker));
+                App.Draw(startMessage);
+                App.Draw(title);
+
+                App.Display();
+            }
+        }
+    }
+}
