3fb3f4f97f02241c8274aab5dd0b22daa964c660
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-04-15 14:12:00 +0200

    added player movements

---
+++ BlocSelector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML;
+using SFML.System;
+using SFML.Graphics;
+
+namespace Spleef
+{
+    public class BlocSelector : Transformable, Drawable
+    {
+        private static Texture texture = new Texture("assets/images/blocSelection.png");
+        private int decal;
+        private RectangleShape shape;
+        private Time timer;
+
+        public BlocSelector()
+        {
+            decal = 0;
+            timer = Time.Zero;
+            shape = new RectangleShape(new Vector2f(64, 128))
+            {
+                Texture = texture,
+                Origin = new Vector2f(0, 64)
+            };
+        }
+
+        public Color Color { get => shape.FillColor; set => shape.FillColor = value; }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            states.Transform *= Transform;
+            target.Draw(shape, states);
+        }
+
+        public void Update(Time delta)
+        {
+            timer += delta;
+            if (timer > Time.FromMilliseconds(150))
+            {
+                timer = Time.Zero;
+                decal++;
+                decal %= 8;
+                shape.TextureRect = new IntRect(decal * 16, 0, 16, 32);
+            }
+        }
+    }
+}
--- Game.cs
+++ Game.cs
@@ -50,60 +50,62 @@ namespace Spleef
                 for (int i = 0; i < nbHoles; i++)
                     tiles[Generator.Next(40), Generator.Next(30)].type = Tile.LAVA;
             }
-            void updateTiles(bool randomize)
+            void updateSingleTile(int x, int y, bool randomize)
             {
                 const int TOP = 1;
                 const int LEFT = 1 << 1;
                 const int BOT = 1 << 2;
                 const int RIGHT = 1 << 3;
+                if (tiles[x, y].type != Tile.LAVA)
+                {
+                    int borders = 0;
+                    if (x > 0 && tiles[x - 1, y].type == Tile.LAVA)
+                        borders |= LEFT;
+                    if (x < 49 && tiles[x + 1, y].type == Tile.LAVA)
+                        borders |= RIGHT;
+                    if (y > 0 && tiles[x, y - 1].type == Tile.LAVA)
+                        borders |= TOP;
+                    if (y < 49 && tiles[x, y + 1].type == Tile.LAVA)
+                        borders |= BOT;
+                    if (borders == LEFT)
+                        tiles[x, y].type = Tile.BORDER_LEFT;
+                    if (borders == RIGHT)
+                        tiles[x, y].type = Tile.BORDER_RIGHT;
+                    if (borders == TOP)
+                        tiles[x, y].type = Tile.BORDER_TOP;
+                    if (borders == BOT)
+                        tiles[x, y].type = Tile.BORDER_BOT;
+                    if (borders == (RIGHT | LEFT))
+                        tiles[x, y].type = Tile.STRAIGHT_VER;
+                    if (borders == (TOP | BOT))
+                        tiles[x, y].type = Tile.STRAIGHT_HOR;
+                    if (borders == (TOP | LEFT))
+                        tiles[x, y].type = Tile.CORNER_TOPLEFT;
+                    if (borders == (BOT | LEFT))
+                        tiles[x, y].type = Tile.CORNER_BOTLEFT;
+                    if (borders == (TOP | RIGHT))
+                        tiles[x, y].type = Tile.CORNER_TOPRIGHT;
+                    if (borders == (BOT | RIGHT))
+                        tiles[x, y].type = Tile.CORNER_BOTRIGHT;
+                    if (borders == (BOT | RIGHT | TOP))
+                        tiles[x, y].type = Tile.TRIBORDER_RIGHT;
+                    if (borders == (BOT | LEFT | TOP))
+                        tiles[x, y].type = Tile.TRIBORDER_LEFT;
+                    if (borders == (RIGHT | LEFT | TOP))
+                        tiles[x, y].type = Tile.TRIBORDER_TOP;
+                    if (borders == (RIGHT | LEFT | BOT))
+                        tiles[x, y].type = Tile.TRIBORDER_BOT;
+                    if (borders == 0b1111)
+                        tiles[x, y].type = Tile.ISLE;
+                }
+                if (randomize)
+                    tiles[x, y].version = Generator.Next(Tile.TilesTextures[tiles[x, y].type].Count);
+            }
+            void updateTiles(bool randomize)
+            {
                 for (int x = 0; x < 40; x++)
                     for (int y = 0; y < 30; y++)
-                    {
-                        if (tiles[x, y].type != Tile.LAVA)
-                        {
-                            int borders = 0;
-                            if (x > 0 && tiles[x - 1, y].type == Tile.LAVA)
-                                borders |= LEFT;
-                            if (x < 49 && tiles[x + 1, y].type == Tile.LAVA)
-                                borders |= RIGHT;
-                            if (y > 0 && tiles[x, y - 1].type == Tile.LAVA)
-                                borders |= TOP;
-                            if (y < 49 && tiles[x, y + 1].type == Tile.LAVA)
-                                borders |= BOT;
-                            if (borders == LEFT)
-                                tiles[x, y].type = Tile.BORDER_LEFT;
-                            if (borders == RIGHT)
-                                tiles[x, y].type = Tile.BORDER_RIGHT;
-                            if (borders == TOP)
-                                tiles[x, y].type = Tile.BORDER_TOP;
-                            if (borders == BOT)
-                                tiles[x, y].type = Tile.BORDER_BOT;
-                            if (borders == (RIGHT | LEFT))
-                                tiles[x, y].type = Tile.STRAIGHT_VER;
-                            if (borders == (TOP | BOT))
-                                tiles[x, y].type = Tile.STRAIGHT_HOR;
-                            if (borders == (TOP | LEFT))
-                                tiles[x, y].type = Tile.CORNER_TOPLEFT;
-                            if (borders == (BOT | LEFT))
-                                tiles[x, y].type = Tile.CORNER_BOTLEFT;
-                            if (borders == (TOP | RIGHT))
-                                tiles[x, y].type = Tile.CORNER_TOPRIGHT;
-                            if (borders == (BOT | RIGHT))
-                                tiles[x, y].type = Tile.CORNER_BOTRIGHT;
-                            if (borders == (BOT | RIGHT | TOP))
-                                tiles[x, y].type = Tile.TRIBORDER_RIGHT;
-                            if (borders == (BOT | LEFT | TOP))
-                                tiles[x, y].type = Tile.TRIBORDER_LEFT;
-                            if (borders == (RIGHT | LEFT | TOP))
-                                tiles[x, y].type = Tile.TRIBORDER_TOP;
-                            if (borders == (RIGHT | LEFT | BOT))
-                                tiles[x, y].type = Tile.TRIBORDER_BOT;
-                            if (borders == 0b1111)
-                                tiles[x, y].type = Tile.ISLE;
-                        }
-                        if (randomize)
-                            tiles[x, y].version = Generator.Next(Tile.TilesTextures[tiles[x, y].type].Count);
-                    }
+                        updateSingleTile(x, y, randomize);
             }
             updateTiles(true);
             var multStates = RenderStates.Default;
@@ -114,7 +116,6 @@ namespace Spleef
             var lightOpacity = 1f;
             var delta = Time.Zero;
             var lightTimer = Time.Zero;
-            var dummyTimer = Time.Zero;
             var gameView = new View(App.GetView());
             var viewObjective = new Vector2f();
             player = new Miner(false);
@@ -135,65 +136,171 @@ namespace Spleef
                     App.SetMouseCursorVisible(true);
                 }
             }
+            var stage = 0;
+            player.Position = new Vector2f(8 * 64, 8 * 64);
+            var selectedBloc = player.Position;
+            var movementAvailable = 0;
+            var stage2Clock = new Clock();
+            Vector2f initialStage2PlayerPos = default;
+            void keyDown(object sender, KeyEventArgs e)
+            {
+                if (stage == 0)
+                {
+                    if (Utilities.IsUpKey(e.Code))
+                        selectedBloc -= new Vector2f(0, 64);
+                    if (Utilities.IsDownKey(e.Code))
+                        selectedBloc += new Vector2f(0, 64);
+                    if (Utilities.IsLeftKey(e.Code))
+                        selectedBloc -= new Vector2f(64, 0);
+                    if (Utilities.IsRightKey(e.Code))
+                        selectedBloc += new Vector2f(64, 0);
+                    selectedBloc.X = Math.Min(39 * 64, selectedBloc.X);
+                    selectedBloc.X = Math.Max(0, selectedBloc.X);
+                    selectedBloc.Y = Math.Min(29 * 64, selectedBloc.Y);
+                    selectedBloc.Y = Math.Max(0, selectedBloc.Y);
+                    if (Utilities.IsConfirmKey(e.Code) && movementAvailable < 2)
+                    {
+                        stage2Clock.Restart();
+                        stage = 1;
+                        initialStage2PlayerPos = player.Position;
+                        updateBreakState();
+                        var currTile = tiles[(int)initialStage2PlayerPos.X >> 6, (int)initialStage2PlayerPos.Y >> 6];
+                        if (currTile.breakState == 0 && currTile.type != Tile.LAVA)
+                            currTile.breakState++;
+                    }
+                }
+            }
             App.MouseButtonPressed += mouseClick;
             App.MouseButtonReleased += mouseRelease;
-            player.Position = new Vector2f(8 * 64, 8 * 64);
+            App.KeyPressed += keyDown;
             gameView.Center = player.Position;
+            void updateBlocGraphics(int x, int y)
+            {
+                var current = tiles[x, y];
+                if (current.breakState >= 4)
+                {
+                    current.type = Tile.LAVA;
+                    current.version = 0;
+                    current.breakState = 0;
+                }
+            }
+            void updateSingleBreakState(int x, int y)
+            {
+                var current = tiles[x, y];
+                if (current.type != Tile.LAVA)
+                {
+                    if (current.breakState > 0)
+                        current.breakState++;
+                    updateBlocGraphics(x, y);
+                    if (current.breakState == 0 && current.type != Tile.LAVA)
+                    {
+                        if (tiles[x - 1, y].type == Tile.LAVA)
+                            current.erosionLevel++;
+                        if (tiles[x + 1, y].type == Tile.LAVA)
+                            current.erosionLevel++;
+                        if (tiles[x, y - 1].type == Tile.LAVA)
+                            current.erosionLevel++;
+                        if (tiles[x, y + 1].type == Tile.LAVA)
+                            current.erosionLevel++;
+                        if (current.erosionLevel >= 4)
+                        {
+                            current.breakState = 1;
+                            current.version = Generator.Next(Tile.TilesTextures[current.type].Count);
+                        }
+                    }
+                }
+            }
             void updateBreakState()
             {
                 for (int x = 2; x < 38; x++)
                     for (int y = 2; y < 28; y++)
-                    {
-                        var current = tiles[x, y];
-                        if (current.type != Tile.LAVA)
-                        {
-                            if (current.breakState == 3)
-                            {
-                                current.type = Tile.LAVA;
-                                current.version = 0;
-                                current.breakState = 0;
-                                continue;
-                            }
-                            if (current.breakState > 0)
-                                current.breakState++;
-                            if (current.breakState == 0)
-                            {
-                                if (tiles[x - 1, y].type == Tile.LAVA)
-                                    current.erosionLevel++;
-                                if (tiles[x + 1, y].type == Tile.LAVA)
-                                    current.erosionLevel++;
-                                if (tiles[x, y - 1].type == Tile.LAVA)
-                                    current.erosionLevel++;
-                                if (tiles[x, y + 1].type == Tile.LAVA)
-                                    current.erosionLevel++;
-                                if (current.erosionLevel >= 4)
-                                {
-                                    current.breakState = 1;
-                                    current.version = Generator.Next(Tile.TilesTextures[current.type].Count);
-                                }
-                            }
-                        }
-                    }
+                        updateSingleBreakState(x, y);
                 updateTiles(false);
             }
+            var selector = new BlocSelector();
             var clock = new Clock();
             while (App.IsOpen)
             {
                 delta = clock.Restart();
                 lightTimer += delta;
-                dummyTimer += delta;
                 App.DispatchEvents();
                 player.Update(delta);
-                viewObjective = player.Position + new Vector2f(32, 32);
+                selector.Update(delta);
+                selector.Position = selectedBloc;
+                if (stage == 0)
+                {
+                    viewObjective = selectedBloc + new Vector2f(32, 32);
+                    if (selector.Position == player.Position + new Vector2f(64, 0)
+                        || selector.Position == player.Position + new Vector2f(-64, 0)
+                        || selector.Position == player.Position + new Vector2f(0, 64)
+                        || selector.Position == player.Position + new Vector2f(0, -64)
+                        || selector.Position == player.Position)
+                        movementAvailable = 0;
+                    else if (selector.Position == player.Position + new Vector2f(128, 0)
+                        || selector.Position == player.Position + new Vector2f(-128, 0)
+                        || selector.Position == player.Position + new Vector2f(0, 128)
+                        || selector.Position == player.Position + new Vector2f(0, -128))
+                        movementAvailable = 1;
+                    else
+                        movementAvailable = 2;
+                    switch (movementAvailable)
+                    {
+                        case 0:
+                            selector.Color = new Color(50, 250, 150);
+                            break;
+
+                        case 1:
+                            selector.Color = new Color(250, 150, 50);
+                            break;
+
+                        case 2:
+                            selector.Color = new Color(250, 50, 150);
+                            break;
+                    }
+                }
+                else if (stage == 1)
+                {
+                    if (movementAvailable == 0)
+                    {
+                        if (stage2Clock.ElapsedTime < Time.FromMilliseconds(250))
+                        {
+                            var perc = stage2Clock.ElapsedTime.AsSeconds() / Time.FromMilliseconds(250).AsSeconds();
+                            var height = (float)Math.Sqrt(1 - 2 * Math.Abs(.5f - perc));
+                            player.Position = initialStage2PlayerPos + perc * (selectedBloc - initialStage2PlayerPos) + new Vector2f(0, -height * 16);
+                        }
+                        else
+                        {
+                            player.Position = selectedBloc;
+                            stage = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (stage2Clock.ElapsedTime < Time.FromMilliseconds(250))
+                        {
+                            var perc = stage2Clock.ElapsedTime.AsSeconds() / Time.FromMilliseconds(250).AsSeconds();
+                            var height = (float)Math.Sqrt(1 - 2 * Math.Abs(.5f - perc));
+                            player.Position = initialStage2PlayerPos + perc * (selectedBloc - initialStage2PlayerPos) + new Vector2f(0, -height * 48);
+                        }
+                        else
+                        {
+                            player.Position = selectedBloc;
+                            stage = 0;
+                            updateSingleBreakState((int)selectedBloc.X >> 6, (int)selectedBloc.Y >> 6);
+                            if (tiles[(int)selectedBloc.X >> 6, (int)selectedBloc.Y >> 6].type == Tile.LAVA)
+                            {
+                                updateSingleTile(((int)selectedBloc.X >> 6) - 1, (int)selectedBloc.Y >> 6, false);
+                                updateSingleTile(((int)selectedBloc.X >> 6) + 1, (int)selectedBloc.Y >> 6, false);
+                                updateSingleTile((int)selectedBloc.X >> 6, ((int)selectedBloc.Y >> 6) + 1, false);
+                                updateSingleTile((int)selectedBloc.X >> 6, ((int)selectedBloc.Y >> 6) - 1, false);
+                            }
+                        }
+                    }
+                }
                 if (lightTimer > Time.FromMilliseconds(120))
                 {
                     lightTimer = Time.Zero;
                     lightOpacity = .8f + (float)Generator.NextDouble() * .2f;
-                }
-                if (dummyTimer > Time.FromSeconds(1))
-                {
-                    updateBreakState();
-                    dummyTimer = Time.Zero;
                 }
                 if (Mouse.IsButtonPressed(Mouse.Button.Right))
                 {
@@ -272,10 +379,13 @@ namespace Spleef
                 App.SetView(App.DefaultView);
                 App.Draw(new Sprite(LightMap.Texture), multStates);
                 App.SetView(gameView);
+                if (stage == 0)
+                    App.Draw(selector);
                 App.Display();
             }
             App.MouseButtonPressed -= mouseClick;
             App.MouseButtonReleased -= mouseRelease;
+            App.KeyPressed -= keyDown;
         }
     }
 }
